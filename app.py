a3d62df68a04fd04f685a93b2b945b7924eb5d34
Alex <forgithub338@gmail.com>
2025-04-15 23:35:44 +0800

    test4

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def handle_message(event):
                 results = cursor.fetchall()
 
                 if results:
-                    reply_lines = [f"{r[0]}｜{r[1]}" for r in results]
+                    reply_lines = [f"{i+1}. {user}｜{game}" for i, (user, game) in enumerate(results)]
                     reply = "目前名單如下：\n" + "\n".join(reply_lines)
                 else:
                     reply = "目前尚無資料。"
